32bb21233bf018dd81f4688dacced612dbe8ce39
Csongor Sohajda <csongorsohajda@Csongors-MacBook-Air.local>
2016-01-17 14:33:17 +0100

    Initial Commit

---
+++ warmup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  warmup
+//
+//  Created by Csongor Sohajda on 17/01/16.
+//  Copyright (c) 2016 Csongor Sohajda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
